a2ed7acc71383c6731b3bfc145c9662c1939ff46
Andriy Starodub <ext-andriy.starodub@here.com>
2019-01-31 19:08:17 +0200

    .

--- admin/show-logs.php
+++ admin/show-logs.php
@@ -9,30 +9,32 @@
         $filename = basename($file);
         $parts = explode('.', $filename);
         $year = $parts[2].".".$parts[1];
+        $day = (int)$parts[0];
 
         if (!isset($logFiles[$year])) $logFiles[$year] = array();
 
-        $logFiles[$year][] = $filename;
+        $logFiles[$year][$day] = $filename;
     }
 
-    echo "<h2>DataBase Logs</h2>";
+    echo "<center><h2>DataBase Logs</h2>";
     echo "<TABLE class='list-content'>";
     foreach ($logFiles as $year => $files) {
+        ksort($files);
         echo "<TR class='list-content'>
                     <TD class='edit-item'><b>$year</b></TD>
                 <TD class='edit-item'>";
-        foreach ($files as $file) {
+        foreach (array_reverse($files) as $file) {
             echo "<a class='file-name' id='$file'>$file</a> &nbsp;";
         }
 
         echo "</TD></TR>";
     }
-    echo "</TABLE>";
+    echo "</TABLE><BR>";
 
     if (isset($_GET['fn'])) {
         $fn = addslashes($_GET['fn']);
         if (strlen($fn) > 14 || strlen($fn) < 12) die();
-        echo "<TABLE class='list-content'>";
+        echo "<h2>$fn</h2><TABLE class='list-content'>";
         $content = file_get_contents('./logs/'.$fn);
         $lines = explode("\n", $content);
         foreach (array_reverse($lines) as $line) {
